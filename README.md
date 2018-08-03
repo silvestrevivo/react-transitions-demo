feafa4b078ac3f830d2cc526e008f2b3e04a8065
Silvestre Vivo Millan <silvestre@realworks.nl>
2018-08-03 16:25:24 +0200

    first simple transition example

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-transitions-demo
-Exercise with React and CSSTransitions following the Udemy tutotial from CodeRevolution
+Exercise with React and CSSTransitions following the Udemy tutotial from CodeRevolution. To install the library:
+
+```
+npm install react-transition-group --save
+```
+This library consists in different modules depending of the complexity of the animationa we want. The most easy one is __Transition__
+
+## Transition
+
+```jxs
+import Transition from 'react-transition-group/Transition'
+```
+
+#### Parameters
+
++ __in__ => is the state of the animation
+
++ __timeout__ => how long takes the animation
+
++ __state__ => _Entering, entered, exiting and exited_
---
+++ src/components/Transition.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Transition from 'react-transition-group/Transition'
+import '../css/App.css'
+
+class TransitionComp extends Component {
+  state = {
+    show: true,
+  }
+
+  showDiv = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    return (
+      <div>
+        {/* {this.state.show ? (
+          <div
+            style={{
+              background: 'red',
+              height: '100px',
+            }}
+          />
+        ) : null} */}
+        <Transition in={this.state.show} timeout={2000}>
+          {state => (
+            <div
+              style={{
+                background: 'red',
+                height: '100px',
+                transition: 'all 2s ease',
+                opacity: state === 'exited' || state === 'exiting' ? 0 : 1,
+              }}
+            >
+              <p>
+                <strong>{state}</strong>
+              </p>
+            </div>
+          )}
+        </Transition>
+        <div onClick={this.showDiv} className="showDiv">
+          Show or hide
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TransitionComp
